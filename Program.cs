16eb52b38a998e01d568c79ccbd5243826f60d57
Addison Moyer <addisonbmoyer@gmail.com>
2016-10-17 11:26:57 -0600

    added some stuff

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        static string myString = "Welcome to My world";
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(myString);
         }
     }
 }
