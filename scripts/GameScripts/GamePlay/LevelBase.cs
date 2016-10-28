037847a2db70acb55e7d0898b278cd752a74390e
Addison Moyer <addisonbmoyer@gmail.com>
2016-10-28 11:46:25 -0600

    calling walk once

--- scripts/GameScripts/GamePlay/LevelBase.cs
+++ scripts/GameScripts/GamePlay/LevelBase.cs
@@ -2,18 +2,22 @@ using System;
 
 public class LevelBase {
 
+    public string StartMessage;
     public bool entranceOpen = true;
 
     public string[] environment = {"Cold", "Wet", "Dark"};
 
     public string[] objects ={"rocks", "ravine", "river","lava"};
 
+    public void Enter (){
+        Console.WriteLine(StartMessage);
+    }
     public void Encounter (int i){
         if (i < objects.Length){
             Console.WriteLine("You've walked into " + objects[i]);
         }
         else{
-            Console.WriteLine("Your path is clear.")
+            Console.WriteLine("Your path is clear.");
         }
     }
 
--- scripts/game.cs
+++ scripts/game.cs
@@ -6,28 +6,29 @@ public class Game {
         Health.power = 100;
         Health.message = "You are getting stronger.";
         Ammo.message = "You have a little bit more ammo.";
-      
-
-    }
-
+        Cave.StartMessage = "You have entered a cave";
+      }
+    
+    // This runs at start of game.
     public void Start (){
-        Health.RunPowerUp();
+        Console.WriteLine("Please type your name");
+        name = Console.ReadLine();
+        Console.WriteLine("Your Player Name is " + name + ".");
+        Walk();
     }
-    /*
-     after promt the game for a name we:
-     enter a cave
-     find Health.
-     meet a Dragon. (need an enimy class)
-     Pick Weapon.
-     Battle Dragon.
-     if we win: get Health and ammo
-     if Dragon wins: loose Health.
-     */
+    
+    private void Walk (){
+        Random randomNum = new Random();
+        Cave.Enter();
+        Cave.Encounter(randomNum.Next(0, Cave.objects.Length));
+    }
+     //Game Levels
+    private LevelBase Cave = new LevelBase();
 
     // game powerup
     public PowerUpBase Health = new PowerUpBase();
     public PowerUpBase Ammo = new PowerUpBase();
-
+    
     // Game weapons
     private WeaponBase Gun = new WeaponBase();
     private WeaponBase Rifle = new WeaponBase();
@@ -37,3 +38,14 @@ public class Game {
 
     private int score;
 }
+
+/*
+     after promt the game for a name we:
+     enter a cave
+     find Health.
+     meet a Dragon. (need an enimy class)
+     Pick Weapon.
+     Battle Dragon.
+     if we win: get Health and ammo
+     if Dragon wins: loose Health.
+     */
